e2f6fc10faeb68a7b7bb043a0b5b924bd2bd0957
JustineGani_GITH <ganijustine8@gmail.com>
2024-09-13 16:26:01 +0800

    Delete asda.js

--- asda.js
+++
@@ -1,8 +0,0 @@
-// Define the actor's full name
-const fullName = "Leonardo DiCaprio"; // Replace with your favorite actor's full name
-
-// Create the message with the full name in uppercase
-const message = `My favorite actor is ${fullName.toUpperCase()}`;
-
-// Log the message to the console
-console.log(message);
